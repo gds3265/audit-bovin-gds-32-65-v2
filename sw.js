6bbe6cc1a74c36a16ffc3081f58fc5fcabd8d87b
gds3265 <elodie.jegou.gds65@reseaugds.com>
2026-09-03 20:51:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'audit-bovin-pwa-v14.6.21.89';
+const CACHE = 'audit-bovin-pwa-v14.6.21.90';
 const ASSETS = [
-  './?v=14.6.21.89','./index.html?v=14.6.21.89','./styles.css?v=14.6.21.89','./app.js?v=14.6.21.89','./cloud-sync.js?v=14.6.21.89',
-  './analysis-rules.js','./knowledge-base.js','./storage.js','./utils.js','./manifest.webmanifest?v=14.6.21.89',
-  './icon-192.png?v=14.6.21.89','./icon-512.png?v=14.6.21.89','./planches-visuelles.png','./jszip.min.js?v=14.6.21.89',
-  './modele-partenaires-passage-bv.xlsx?v=14.6.21.89','./questionnaire.html?v=14.6.21.89','./questionnaire.js?v=14.6.21.89','./questionnaire.css?v=14.6.21.89',
+  './?v=14.6.21.90','./index.html?v=14.6.21.90','./styles.css?v=14.6.21.90','./app.js?v=14.6.21.90','./cloud-sync.js?v=14.6.21.90',
+  './analysis-rules.js','./knowledge-base.js','./storage.js','./utils.js','./manifest.webmanifest?v=14.6.21.90',
+  './icon-192.png?v=14.6.21.90','./icon-512.png?v=14.6.21.90','./planches-visuelles.png','./jszip.min.js?v=14.6.21.90',
+  './modele-partenaires-passage-bv.xlsx?v=14.6.21.90','./questionnaire.html?v=14.6.21.90','./questionnaire.js?v=14.6.21.90','./questionnaire.css?v=14.6.21.90',
   './memo-aplombs.jpeg','./memo-srr.jpeg','./memo-nec.jpeg','./outil-apera-ph5f.pdf','./outil-control-th.pdf','./outil-extech-ph100.pdf','./outil-extech-re300.pdf',
   './outil-freestyle-optium.pdf','./outil-hanna-hi701.pdf','./outil-laquatwin-4m.pdf','./outil-lysun-uree.pdf','./outil-multimetre.pdf',
   './theme-approche-globale.pdf','./theme-eau-abreuvement.pdf','./theme-feces-digestion.pdf','./theme-fourrages-alimentation.pdf','./theme-lait-colostrum-veaux.pdf',
@@ -34,7 +34,7 @@ self.addEventListener('fetch', event => {
       return response;
     }catch(_){
       const hit=await caches.match(req);if(hit)return hit;
-      if(req.mode==='navigate')return (await caches.match('./index.html?v=14.6.21.89'))||(await caches.match('./?v=14.6.21.89'))||(await caches.match('./index.html'));
+      if(req.mode==='navigate')return (await caches.match('./index.html?v=14.6.21.90'))||(await caches.match('./?v=14.6.21.90'))||(await caches.match('./index.html'));
       return Response.error();
     }
   })());
